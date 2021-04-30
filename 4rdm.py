4ce3d2ab06b72b58ec85659ac9056321ac5d3466
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-04-30 23:50:19 +0200

    less code

--- 4rdm.py
+++ 4rdm.py
@@ -1,23 +1,19 @@
 import os, winshell
 import requests
-directory = "4RDM"
-path = os.path.join(os.environ["ProgramFiles"], "4RDM")
+
 try:
-    os.mkdir(path)
+    os.mkdir(os.path.join(os.environ["ProgramFiles"], "4RDM"))
 except:
     pass
-url = 'https://4rdm.cf/4rdmlogo.ico'
-r = requests.get(url, allow_redirects=True)
-a = os.environ["ProgramFiles"]
-open('{essa}\\4RDM\\4rdmlogo.ico'.format(essa = a), 'wb').write(r.content)
 
-te = ["ProgramFiles"]
-print(os.getenv("SystemDrive"))
+r = requests.get('https://4rdm.cf/4rdmlogo.ico', allow_redirects=True)
+open('{essa}\\4RDM\\4rdmlogo.ico'.format(essa = os.environ["ProgramFiles"]), 'wb').write(r.content)
+
 desktop = winshell.desktop()
 path = os.path.join(desktop, "4RDM.CF.url")
 target = "fivem://connect/4rdm.cf"
 shortcut = open(path, 'w')
 shortcut.write('[InternetShortcut]\n')
-a = ('URL=fivem://connect/4rdm.cf\nIconIndex=0\nHotKey=0\nIDList=\nIconFile={drive}\\Program Files\\4RDM\\4rdmlogo.ico\n').format(drive = os.getenv("SystemDrive"))
+a = ('URL=fivem://connect/4rdm.cf\nIconIndex=0\nHotKey=0\nIDList=\nIconFile={a}\\4RDM\\4rdmlogo.ico\n').format(a = os.environ["ProgramFiles"])
 shortcut.write(a)
 shortcut.close()
